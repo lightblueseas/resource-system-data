5e1f2e093bf418c1ca7ce52c6e775bbf2bbea1fc
astrapi69 <asterios.raptis@gmx.net>
2015-11-30 18:42:29 +0100

    version update after refactoring.

--- resource-system-init/src/main/java/de/alpharogroup/db/init/InitializeDatabase.java
+++ resource-system-init/src/main/java/de/alpharogroup/db/init/InitializeDatabase.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import de.alpharogroup.lang.PropertiesUtils;
+import de.alpharogroup.lang.PropertiesExtensions;
 import de.alpharogroup.resource.system.db.init.DatabaseInitialization;
 
 /**
@@ -20,7 +20,7 @@ public class InitializeDatabase {
 	 */
 	public static void main(final String[] args) throws ClassNotFoundException,
 			SQLException, IOException {
-		Properties dbProperties = PropertiesUtils.loadProperties("jdbc.properties");
+		Properties dbProperties = PropertiesExtensions.loadProperties("jdbc.properties");
 		new DatabaseInitialization(dbProperties).initializeDatabase();
 	}
 
