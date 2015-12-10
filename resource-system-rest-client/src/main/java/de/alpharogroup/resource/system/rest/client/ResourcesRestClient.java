568b0c86e15093b773465e3b6152f44871be8153
astrapi69 <asterios.raptis@gmx.net>
2015-12-10 17:33:47 +0100

    parent pom version update for extend from abstract rest client

--- resource-system-rest-client/src/main/java/de/alpharogroup/resource/system/rest/client/ResourcesRestClient.java
+++ resource-system-rest-client/src/main/java/de/alpharogroup/resource/system/rest/client/ResourcesRestClient.java
@@ -1,32 +1,13 @@
 package de.alpharogroup.resource.system.rest.client;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.ws.rs.core.MediaType;
-
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
-import org.apache.cxf.jaxrs.client.WebClient;
-
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-
+import de.alpharogroup.cxf.rest.client.AbstractRestClient;
 import de.alpharogroup.resource.system.rest.api.ResourcesResource;
 import lombok.Getter;
 
 /**
  * The class {@link ResourcesRestClient}.
  */
-public class ResourcesRestClient {
-
-	/** The Constant DEFAULT_HTTP_PORT. */
-	public static final int DEFAULT_HTTP_PORT = 8080;
-
-	/** The Constant BASE_URL_PREFIX. */
-	public static final String BASE_URL_PREFIX = "http://localhost";
-
-	/** The Constant DEFAULT_BASE_URL. */
-	public static final String DEFAULT_BASE_URL = BASE_URL_PREFIX + ":" + DEFAULT_HTTP_PORT;
-	
+public class ResourcesRestClient extends AbstractRestClient {
 
 	/**
 	 * Gets the resources resource.
@@ -48,12 +29,7 @@ public class ResourcesRestClient {
 	 * @param baseUrl the base url
 	 */
 	public ResourcesRestClient(final String baseUrl) {
-
-        List<Object> providers = new ArrayList<>();
-        providers.add(new JacksonJsonProvider());
-        
-        resourcesResource  = JAXRSClientFactory.create(baseUrl, ResourcesResource.class, providers);
-        WebClient.client(resourcesResource).accept(MediaType.APPLICATION_JSON);
-        WebClient.client(resourcesResource).type(MediaType.APPLICATION_JSON);
+		super(baseUrl);
+        resourcesResource  = newResource(ResourcesResource.class);
 	}
 }
