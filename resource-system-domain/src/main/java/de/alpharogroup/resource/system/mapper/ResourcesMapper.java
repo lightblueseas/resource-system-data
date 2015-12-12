2dca1481af75bc419bd6706b3734f1258f857e55
astrapi69 <asterios.raptis@gmx.net>
2015-12-12 14:14:21 +0100

    Mapper updated after refactoring.

--- resource-system-domain/src/main/java/de/alpharogroup/resource/system/mapper/ResourcesMapper.java
+++ resource-system-domain/src/main/java/de/alpharogroup/resource/system/mapper/ResourcesMapper.java
@@ -1,9 +1,9 @@
 package de.alpharogroup.resource.system.mapper;
 
-import de.alpharogroup.db.entitymapper.AbstractEntityBOMapper;
+import de.alpharogroup.db.entitymapper.AbstractEntityDOMapper;
 import de.alpharogroup.resource.system.domain.Resource;
 import de.alpharogroup.resource.system.entities.Resources;
 
-public class ResourcesMapper extends AbstractEntityBOMapper<Resources, Resource> {
+public class ResourcesMapper extends AbstractEntityDOMapper<Resources, Resource> {
 
 }
