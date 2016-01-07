dec34fdc4dcb56da4cba5ca12b43cfc1e285ce7e
astrapi69 <asterios.raptis@gmx.net>
2016-01-07 17:58:45 +0100

    hql statements adapted to new entity class names

--- resource-system-business/src/main/java/de/alpharogroup/resource/system/service/util/HqlStringCreator.java
+++ resource-system-business/src/main/java/de/alpharogroup/resource/system/service/util/HqlStringCreator.java
@@ -1,16 +1,18 @@
 package de.alpharogroup.resource.system.service.util;
 
+import de.alpharogroup.resource.system.entities.Resources;
+
 public class HqlStringCreator {
-	public static String forResources(String description, String filename,
-			String filesize, String contentType) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("select r from Resources r");
-		boolean descriptionIsNotNull = description != null && !description.isEmpty();
+	public static String forResources(final String description, final String filename,
+			final String filesize, final String contentType) {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("select r from " + Resources.class.getSimpleName() + " r");
+		final boolean descriptionIsNotNull = description != null && !description.isEmpty();
 		if (descriptionIsNotNull) {
 			sb.append(" ");
 			sb.append("where r.description=:description");
 		}
-		boolean filenameIsNotNull = filename != null && !filename.isEmpty();
+		final boolean filenameIsNotNull = filename != null && !filename.isEmpty();
 		if (filenameIsNotNull) {
 			sb.append(" ");
 			if (descriptionIsNotNull) {
@@ -19,16 +21,16 @@ public class HqlStringCreator {
 				sb.append("where r.filename=:filename");
 			}
 		}
-		boolean filesizeIsNotNull = filesize != null && !filesize.isEmpty();
+		final boolean filesizeIsNotNull = filesize != null && !filesize.isEmpty();
 		if(filesizeIsNotNull) {
 			sb.append(" ");
 			if(!descriptionIsNotNull && !filenameIsNotNull) {
-				sb.append("where r.filesize=:filesize");				
+				sb.append("where r.filesize=:filesize");
 			} else {
-				sb.append("and r.filesize=:filesize");					
-			}			
+				sb.append("and r.filesize=:filesize");
+			}
 		}
-		boolean contentTypeIsNotNull = contentType != null && !contentType.isEmpty();
+		final boolean contentTypeIsNotNull = contentType != null && !contentType.isEmpty();
 		if (contentTypeIsNotNull) {
 			sb.append(" ");
 			if (!descriptionIsNotNull && !filenameIsNotNull
@@ -40,5 +42,5 @@ public class HqlStringCreator {
 		}
 		return sb.toString();
 	}
-	
+
 }
