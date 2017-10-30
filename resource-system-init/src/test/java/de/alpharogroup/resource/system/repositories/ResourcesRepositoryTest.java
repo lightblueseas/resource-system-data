2191bf6cdbb299b1f4d661d52453eb21f9a84b84
Asterios Raptis <asterios.raptis@gmx.net>
2017-10-30 12:56:42 +0100

    moved unit test to appropriate package

--- resource-system-init/src/test/java/de/alpharogroup/resource/system/repositories/ResourcesRepositoryTest.java
+++ resource-system-init/src/test/java/de/alpharogroup/resource/system/repositories/ResourcesRepositoryTest.java
@@ -22,7 +22,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package de.alpharogroup.resource.system.daos;
+package de.alpharogroup.resource.system.repositories;
 
 import java.util.List;
 
